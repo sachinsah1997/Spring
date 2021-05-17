9521d82cff6a5ceefc9769624efbdd0d3cb07c26
sachinsah1997 <ssk1222014@gmail.com>
2021-05-17 10:47:16 +0530

    Working with SpEL

--- springcore/src/main/java/com/sachinsah/springcore/sterotype/Fruit.java
+++ springcore/src/main/java/com/sachinsah/springcore/sterotype/Fruit.java
@@ -13,6 +13,40 @@ public class Fruit {
 	@Value("Mango")
 	private String name;
 	
+	@Value("#{T(java.lang.Math).sqrt(144)}")
+	private int price;
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	@Value("#{new java.lang.String('seasonal')}")
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+	
+	@Value("#{7>2}")
+	private boolean isAvailable;
+
+	
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
 	@Value("#{phonNumberList}")
 	private List<String> types;
 	
@@ -24,11 +58,7 @@ public class Fruit {
 		this.types = types;
 	}
 
-	@Override
-	public String toString() {
-		return "Fruit [name=" + name + "]";
-	}
-
+	
 	public String getName() {
 		return name;
 	}
@@ -36,4 +66,12 @@ public class Fruit {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	@Override
+	public String toString() {
+		return "Fruit [name=" + name + ", price=" + price + ", type=" + type + ", isAvailable=" + isAvailable
+				+ ", types=" + types + "]";
+	}
+	
+	
 }
