3ce6d074fd12f81e4913f7d0a49c00a307d81d52
sachinsah1997 <ssk1222014@gmail.com>
2021-05-20 17:19:45 +0530

    Intial commit with fully functioning with server

---
+++ springboot/src/test/java/com/sachinsah/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sachinsah.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.print(true);
+	}
+
+}
