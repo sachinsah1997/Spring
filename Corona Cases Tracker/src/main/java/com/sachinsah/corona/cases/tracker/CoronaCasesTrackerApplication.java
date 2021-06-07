7d053b383eb42f498c9186d8c58b78b860f5744b
sachinsah1997 <ssk1222014@gmail.com>
2021-06-07 08:20:34 +0530

    intial commit

---
+++ Corona Cases Tracker/src/main/java/com/sachinsah/corona/cases/tracker/CoronaCasesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sachinsah.corona.cases.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaCasesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaCasesTrackerApplication.class, args);
+	}
+
+}
---
+++ Corona Cases Tracker/src/test/java/com/sachinsah/corona/cases/tracker/CoronaCasesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinsah.corona.cases.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaCasesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
