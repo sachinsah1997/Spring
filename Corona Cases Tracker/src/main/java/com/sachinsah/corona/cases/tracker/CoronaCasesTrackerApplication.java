bf32d502373e91ebe7694f136b88a498ac10d1b2
sachinsah1997 <ssk1222014@gmail.com>
2021-06-07 09:29:29 +0530

    Retreive data from csv and printed on console

--- Corona Cases Tracker/src/main/java/com/sachinsah/corona/cases/tracker/CoronaCasesTrackerApplication.java
+++ Corona Cases Tracker/src/main/java/com/sachinsah/corona/cases/tracker/CoronaCasesTrackerApplication.java
@@ -2,12 +2,12 @@ package com.sachinsah.corona.cases.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CoronaCasesTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoronaCasesTrackerApplication.class, args);
+		SpringApplication.run(CoronaCasesTrackerApplication.class, args);	
 	}
-
 }
