9be06318f851d9096a0fb495b1efb60e28676672
sachinsah1997 <ssk1222014@gmail.com>
2021-06-07 19:38:47 +0530

    Given new Api name and some css modification

--- Corona Cases Tracker/src/main/java/com/sachinsah/corona/cases/tracker/controller/CoronaCasesController.java
+++ Corona Cases Tracker/src/main/java/com/sachinsah/corona/cases/tracker/controller/CoronaCasesController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import com.sachinsah.corona.cases.tracker.models.CoronaCasesData;
 import com.sachinsah.corona.cases.tracker.services.CoronaCasesDataService;
 
 @Controller
@@ -13,9 +13,17 @@ public class CoronaCasesController {
 	@Autowired
 	CoronaCasesDataService coronaCasesDataService;
 	
-	@GetMapping("/")
+	@GetMapping("/coronatrackerapp")
 	public String controllerMethod(Model model) {
 		
+		double sum=0;
+		for(CoronaCasesData ccd : coronaCasesDataService.getAllStats()) {
+			if(ccd.getNewCases() != "") {
+				sum += Double.parseDouble(ccd.getNewCases());
+			}
+		}
+		
+		model.addAttribute("todayCase", sum);
 		model.addAttribute("stats", coronaCasesDataService.getAllStats());
 		return "index";
 	}
