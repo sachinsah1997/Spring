439fc47e1fc03fcee8430b97db76489064e8867c
sachinsah1997 <ssk1222014@gmail.com>
2021-06-07 14:25:38 +0530

    Add model for passing value from controller to html page  - done testing and it's working fine

--- Corona Cases Tracker/src/main/java/com/sachinsah/corona/cases/tracker/controller/CoronaCasesController.java
+++ Corona Cases Tracker/src/main/java/com/sachinsah/corona/cases/tracker/controller/CoronaCasesController.java
@@ -1,13 +1,17 @@
 package com.sachinsah.corona.cases.tracker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CoronaCasesController {
 	
 	@GetMapping("/")
-	public String controllerMethod() {
+	public String controllerMethod(Model model) {
+		
+		model.addAttribute("param1", "Sachin Sah");
+		
 		return "index";
 	}
 	  
